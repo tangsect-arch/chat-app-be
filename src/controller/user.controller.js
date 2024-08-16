eb5c646368e383553124d2f3b885fb0f03436504
Sabari <sabari288@gmail.com>
2024-08-16 18:53:42 +0530

    User crud operation

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,10 +1,25 @@
-// Import required modules
 const UserModel = require("../model/user.model");
+const crypto = require("crypto");
 
-// Create a new user
+const salt = process.env.SALT;
+
+const hashPassword = (password) => {
+  const hash = crypto.createHmac("sha256", salt);
+  hash.update(password);
+  return { hash: hash.digest("hex") };
+};
+
+const verifyPassword = (password, hash) => {
+  const newHash = crypto.createHmac("sha256", salt);
+  newHash.update(password);
+  return newHash.digest("hex") === hash;
+};
+
 exports.createUser = async (req, res) => {
   try {
     const userData = req.body;
+    const { hash } = hashPassword(userData.password);
+    userData.password = hash;
     const user = await UserModel.createUser(userData);
     res.status(201).json(user);
   } catch (error) {
@@ -63,6 +78,23 @@ exports.deleteUser = async (req, res) => {
     } else {
       res.status(200).json({ message: "User deleted" });
     }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const userData = req.body;
+    const user = await UserModel.getUserByUsername(userData.username);
+    if (!user) {
+      return res.status(401).json({ error: "Invalid username or password" });
+    }
+    const isValid = verifyPassword(userData.password, user.password.hash);
+    if (!isValid) {
+      return res.status(401).json({ error: "Invalid username or password" });
+    }
+    res.status(200).json({ message: "Login successful" });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
